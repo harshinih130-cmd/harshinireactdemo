0d54b6239fc8714930a3b3e4d5b0a92e6774f9a5
Harshini <harshinih130@gmail.com>
2025-12-15 13:18:32 +0530

    Next Basics

--- src/App.js
+++ src/App.js
@@ -1,27 +1,133 @@
-import React from "react";
-class App extends React.Component
+function App()
 {
-  render()
+  if(true)
   {
-    return(<>
-          <h1>Welcome to Class Components</h1>
-          <h1>Welcome to Class Components</h1>
-          <h1>Welcome to Class Components</h1>
-          <h1>Welcome to Class Components</h1>
-          <p>Aute dolore occaecat officia esse voluptate reprehenderit reprehenderit eu elit nisi aliquip do laborum. Et fugiat commodo non et sunt aute tempor est velit quis commodo reprehenderit culpa non. Commodo et in ex ipsum sunt. Consectetur deserunt minim id tempor nulla nulla quis Lorem laborum dolore excepteur cupidatat fugiat. Eu amet Lorem proident anim officia aliquip cillum esse proident ex. Quis fugiat mollit nisi nostrud est laborum adipisicing ad.
-
-Deserunt nisi non deserunt veniam amet labore esse culpa velit quis. Ut aute eu eiusmod velit consequat culpa esse excepteur. Dolore laborum aliquip elit velit nulla. Irure duis enim nostrud do Lorem est nulla sunt culpa deserunt esse. Dolor pariatur id mollit ex.
-
-Aliqua sunt nulla reprehenderit incididunt. Ex sit veniam anim ipsum ex irure dolore exercitation non excepteur dolore exercitation. Duis elit enim nisi consequat commodo enim culpa Lorem veniam Lorem sunt do anim.
-
-Sunt enim id ipsum et elit ipsum excepteur adipisicing officia. Pariatur in tempor anim incididunt aute ipsum eu deserunt. Duis enim deserunt officia do dolore sunt cillum qui exercitation officia sunt.
-
-Fugiat officia aliquip quis sunt incididunt veniam non magna excepteur. Pariatur cupidatat reprehenderit nulla aliqua nisi ipsum adipisicing ullamco do aliquip veniam eiusmod non et. Mollit aliqua non nulla duis voluptate nostrud reprehenderit dolore nulla. Dolore id fugiat enim qui ea tempor nulla elit qui. Cupidatat non cupidatat non cupidatat ullamco sint nisi ipsum cupidatat proident duis amet.o\p</p>
-          </>)
-    
+    var v=100
+    let l=200
+    const c=300
+    v++;
+    l++;
+    console.log("Let:"+1);
+    // c++
+    console.log("const:"+c);
   }
+  return(
+    <>
+       <h1>variable : global :{v}</h1>
+       <h1>does not work let and const : because local</h1>
+    </>
+  )
 }
 export default App
+
+
+
+
+
+// function App()
+// {
+//   let myfun=(a,b,c)=>{
+//     var tot=a+b+c
+//     document.getElementById("res").innerHTML=`<h2>Total:${tot}</h2>`;
+
+//   }
+//   return(<>
+//       <h2>Arrow Function Without parameters</h2>
+//       <h3>Note: onClick C must be caps</h3>
+//       <h3>Note: in calling function don't use parenthesis when without passing args</h3>
+//       <button onClick={()=>myfun(10,20,30)}>click me</button>
+//       <div id="res"></div>
+//       </>)
+// }
+// export default App 
+
+
+
+
+
+// import myimg from'./images/image1.jpeg'
+// function App()
+// {
+//   return(<>
+//       <h2>Hello Teddy</h2>
+//       <img src={myimg} width="30%" height="auto" alt="path is wrong"></img>
+//       </>)
+// }
+// export default App
+
+
+// function App()
+// {
+//   var a=600
+//   var b=200
+//   var c=800
+//   return(
+//     <>
+//         <h2>Biggest Among 2 no's using conditional or ternaray operator</h2>
+//         <h1>Biggest Number a or b:{a>b?a:b}</h1>
+//         <h1>Biggest Number a or b or c:{(a>b && a>c) ?a:(b>c?b:c)}</h1>
+//     </>    
+       
+       
+//   )
+
+// }
+// export default App
+// function App(){
+//   // javascript learned code
+//   var a=10
+//   var b=20
+//   var c="Harshini"
+
+//   return(<>
+//          <h1>Total:{a+b}</h1>
+//          <marquee bgcolor='pink'><font size='7'>{c}</font></marquee>
+//          </>)
+// }
+// export default App
+
+// import React from "react";
+// class Parent extends React.Component
+// {
+//   render()
+//   {
+//        return(<>
+//        <h1>This is parent class</h1>
+//         <h1>This is parent class</h1>
+//          <h1>This is parent class</h1>
+//          <h1>This is parent class</h1>
+//          <h1>This is parent class</h1>
+//        </>
+//        )
+//   }
+// }
+// class App extends Parent
+// {
+//   render()
+
+//   {
+//     return(<>
+//     <Parent/>
+//     <h1>Welcome to class components</h1>
+//      <h1>Welcome to class components</h1>
+//       <h1>Welcome to class components</h1>
+//        <h1>Welcome to class components</h1>
+//        <hr></hr>
+//        <p>In adipisicing veniam est do et nostrud. Dolor aliquip veniam nulla incididunt commodo voluptate adipisicing dolore id commodo. Duis magna quis amet velit nostrud esse anim id dolore enim et. Sint esse commodo ad do eu. Officia cupidatat velit pariatur ad do tempor ullamco laboris consequat adipisicing ullamco ullamco ut.
+
+// Non tempor eu quis exercitation. Officia anim consequat veniam ipsum dolore nisi veniam fugiat. Cupidatat anim ipsum ad esse minim enim est do irure laboris tempor consectetur elit anim. Sit sunt exercitation aliqua laborum occaecat in labore elit dolore veniam qui adipisicing nostrud. Elit nulla ea elit dolore aute minim labore exercitation.
+
+// Cillum ad sint laboris labore elit ut culpa irure incididunt. Dolor elit voluptate eu Lorem sint ut ad duis voluptate. Laborum qui et ullamco aliquip cupidatat cillum esse do deserunt veniam.
+
+// Sunt qui quis ex adipisicing ad duis et incididunt. Tempor est incididunt qui quis. Amet occaecat nostrud anim voluptate nostrud laboris commodo aliqua dolor sunt. Ad magna velit qui laborum officia sit aliqua. Aute occaecat ad et eu aute fugiat minim do aliquip non laborum. Ad velit laborum sit do dolor commodo in. Minim commodo elit sit minim minim.
+
+// Pariatur adipisicing nostrud sint elit minim enim deserunt officia anim sint. Reprehenderit aliquip enim esse esse elit anim quis officia enim est. Aliqua deserunt velit velit deserunt excepteur ad id nostrud occaecat consequat officia esse veniam. Et officia incididunt esse cupidatat laboris in ipsum consequat consequat. Cillum nisi ullamco dolore officia dolore in.</p>
+//     </>
+
+//     )
+//   }
+// }
+// export default App 
 
 // function App()
 // {
